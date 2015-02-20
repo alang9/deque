52766a57b6dd96b6df8f2d148ffa476e10e3541c
Alex Lang <me@alang.ca>
2015-02-20 13:55:01 -0800

    Invert the thristing direction

--- Data/Deque/NonCat.hs
+++ Data/Deque/NonCat.hs
@@ -12,18 +12,18 @@ type F = False
 data Buffer u v w x y z q i j where
   B0 :: Buffer T F F F F F q i i
   B1 :: !(q i j) -> Buffer F T F F F F q i j
-  B2 :: !(q i j) -> !(q j k) -> Buffer F F T F F F q i k
-  B3 :: !(q i j) -> !(q j k) -> !(q k l) -> Buffer F F F T F F q i l
-  B4 :: !(q i j) -> !(q j k) -> !(q k l) -> !(q l m) -> Buffer F F F F T F q i m
-  B5 :: !(q i j) -> !(q j k) -> !(q k l) -> !(q l m) -> !(q m n) -> Buffer F F F F F T q i n
+  B2 :: !(q j k) -> !(q i j) -> Buffer F F T F F F q i k
+  B3 :: !(q k l) -> !(q j k) -> !(q i j) -> Buffer F F F T F F q i l
+  B4 :: !(q l m) -> !(q k l) -> !(q j k) -> !(q i j) -> Buffer F F F F T F q i m
+  B5 :: !(q m n) -> !(q l m) -> !(q k l) -> !(q j k) -> !(q i j) -> Buffer F F F F F T q i n
 
 data Pair q i k where
-  P :: !(q i j) -> !(q j k) -> Pair q i k
+  P :: !(q j k) -> !(q i j) -> Pair q i k
 
 data HPair q r i k where
-  H :: !(q i j) -> !(r j k) -> HPair q r i k
+  H :: !(q j k) -> !(r i j) -> HPair q r i k
 
-pushB :: q i j -> Buffer u v w x y F q j k -> Buffer F u v w x y q i k
+pushB :: q j k -> Buffer u v w x y F q i j -> Buffer F u v w x y q i k
 pushB a B0 = B1 a
 pushB a (B1 b) = B2 a b
 pushB a (B2 b c) = B3 a b c
@@ -37,7 +37,7 @@ popB (B3 a b c) = a `H` B2 b c
 popB (B4 a b c d) = a `H` B3 b c d
 popB (B5 a b c d e) = a `H` B4 b c d e
 
-pushB2 :: Pair q i j -> Buffer u v w x F F q j k -> Buffer F F u v w x q i k
+pushB2 :: Pair q j k -> Buffer u v w x F F q i j -> Buffer F F u v w x q i k
 pushB2 (P a b) cs = pushB a (pushB b cs)
 
 popB2 :: Buffer F F w x y z q i j -> HPair (Pair q) (Buffer w x y z F F q) i j
@@ -46,7 +46,7 @@ popB2 as =
     a `H` bs -> case popB bs of
       b `H` cs -> P a b `H` cs
 
-injectB :: Buffer u v w x y F q i j -> q j k -> Buffer F u v w x y q i k
+injectB :: Buffer u v w x y F q j k -> q i j -> Buffer F u v w x y q i k
 injectB B0 a = B1 a
 injectB (B1 a) b = B2 a b
 injectB (B2 a b) c = B3 a b c
@@ -60,7 +60,7 @@ ejectB (B3 a b c) = B2 a b `H` c
 ejectB (B4 a b c d) = B3 a b c `H` d
 ejectB (B5 a b c d e) = B4 a b c d `H` e
 
-injectB2 :: Buffer u v w x F F q i j -> Pair q j k -> Buffer F F u v w x q i k
+injectB2 :: Buffer u v w x F F q j k -> Pair q i j -> Buffer F F u v w x q i k
 injectB2 as (P b c) = injectB (injectB as b) c
 
 ejectB2 :: Buffer F F w x y z q i j -> HPair (Buffer w x y z F F q) (Pair q) i j
@@ -84,11 +84,11 @@ overUnder (B5 a b c d e) = Over (B5 a b c d e)
 data Nope i j where
 
 data Fringe c q i j k l where
-  RX :: !(Buffer s F F F F t q i j) -> !(Buffer u v w x y z q k l) -> Fringe Red q i j k l
-  XR :: !(Buffer F u v w x F q i j) -> !(Buffer y F F F F z q k l) -> Fringe Red q i j k l
-  YX :: !(Buffer F u F F v F q i j) -> !(Buffer F w x y z F q k l) -> Fringe Yellow q i j k l
-  GY :: !(Buffer F F w x F F q i j) -> !(Buffer F y F F z F q k l) -> Fringe Yellow q i j k l
-  GG :: !(Buffer F F w x F F q i j) -> !(Buffer F F y z F F q k l) -> Fringe Green q i j k l
+  RX :: !(Buffer s F F F F t q k l) -> !(Buffer u v w x y z q i j) -> Fringe Red q i j k l
+  XR :: !(Buffer F u v w x F q k l) -> !(Buffer y F F F F z q i j) -> Fringe Red q i j k l
+  YX :: !(Buffer F u F F v F q k l) -> !(Buffer F w x y z F q i j) -> Fringe Yellow q i j k l
+  GY :: !(Buffer F F w x F F q k l) -> !(Buffer F y F F z F q i j) -> Fringe Yellow q i j k l
+  GG :: !(Buffer F F w x F F q k l) -> !(Buffer F F y z F F q i j) -> Fringe Green q i j k l
 
 data Yellows q r i j k l where
   N :: Yellows q q i i l l
@@ -121,7 +121,7 @@ implant (Y1 b@B1{}) _ = Deque $ TinyL b -- in agda we could properly eliminate t
 implant (Y1 b@B4{}) _ = Deque $ TinyH b
 implant (Y y ys) z = Deque $ BigY y ys z
 
-push :: q i j -> Deque q j k -> Deque q i k
+push :: q j k -> Deque q i j -> Deque q i k
 push a (Deque Empty) = Deque $ TinyL (B1 a)
 push a (Deque (TinyL (B1 b))) = Deque $ TinyL (B2 a b)
 push a (Deque (TinyL (B2 b c))) = Deque $ TinyH (B3 a b c)

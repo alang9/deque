9d0eba7b850b07ccc51f9099b39eacb1f6c45b36
Alex Lang <me@alang.ca>
2015-03-12 14:16:43 -0700

    Change the type of onlyL' and onlyR' to handle Caps instead of Deques

--- Data/Deque/Cat.hs
+++ Data/Deque/Cat.hs
@@ -482,12 +482,12 @@ catenate' (DOR (Triple (O0 bl)))      (DOL (Triple (O0 br)))      f = f $ DOL (T
 catenate' (DOR (Triple (O0 bl@B8{}))) (DOR (Triple (O0 br@B8{}))) f = f $ DOL (Triple (OGG bl D0 br))
 catenate' (DOR (Triple (O0 bl)))      (DOR (Triple (O0 br)))      f = f $ DOL (Triple (O0 (catenateB bl br)))
 -- -- Case 2:
-catenate' (DOL (Triple (O0 bl))) d f = onlyL' bl d f
-catenate' (DOR (Triple (O0 bl))) d f = onlyL' bl d f
+catenate' (DOL (Triple (O0 bl))) (D2 lt rt) f = onlyL' bl lt (f . (flip D2 rt))
+catenate' (DOR (Triple (O0 bl))) (D2 lt rt) f = onlyL' bl lt (f . (flip D2 rt))
 -- -- Case 3
-catenate' d (DOL (Triple (O0 br))) f = onlyR' d br f
-catenate' d (DOR (Triple (O0 br))) f = onlyR' d br f
--- -- Case 1
+catenate' (D2 lt rt) (DOL (Triple (O0 br))) f = onlyR' rt br (f . D2 lt)
+catenate' (D2 lt rt) (DOR (Triple (O0 br))) f = onlyR' rt br (f . D2 lt)
+-- Case 1
 -- catenate' d e f = f $ D2 (fixLeft d) (fixRight e)
 
 onlyL :: Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q j k -> Cap LeftTriple (Closed Green) q i j -> Cap LeftTriple (Closed Green) q i k
@@ -511,44 +511,41 @@ onlyL bl@B7{} (Cap (LO ll d lr) cap1)          = Triple (LG (catenateB bl ll) d2
 onlyL bl      (Cap (LY ll d lr) cap1)          = Triple (LG (catenateB bl ll) d2 lr) where d2 = plugL cap1 d
 onlyL bl      (Triple (LG ll d lr))            = Triple (LG (catenateB bl ll) d lr)
 
-onlyL' :: Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q j k -> Deque (Closed cl1) (Closed cr1) q i j -> (forall cl2 cr2. Deque (Closed cl2) (Closed cr2) q i k -> g) -> g
-onlyL' _ D0 _ = error "Impossible"
-onlyL' _ DOL{} _ = error "Impossible"
-onlyL' _ DOR{} _ = error "Impossible"
-onlyL' bl@B8{} (D2 (Triple (L0 ll lr)) r)              f = f $ D2 (Triple (LG bl (push (S1 ll) D0) lr)) r
-onlyL' bl@B8{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (pushWith (S1 ll) (plugR d cap1) (\e -> LG bl e lr))) r
-onlyL' bl@B8{} (D2 (Cap (LY ll d lr) cap1) r)          f = f $ D2 (Triple (pushWith (S1 ll) (plugL cap1 d) (\e -> LG bl e lr))) r
-onlyL' bl@B8{} (D2 (Triple (LG ll d lr)) r)            f = f $ D2 (Triple (pushWith (S1 ll) d (\e -> LG bl e lr))) r
-onlyL' bl@B8{} (D2 (Triple (LR ll d lr)) r)            f = f $ D2 (Triple (pushWith (S1 ll) d (\e -> LG bl e lr))) r
-onlyL' bl@B9{} (D2 (Triple (L0 ll lr)) r)              f = f $ D2 (Triple (LG bl (push (S1 ll) D0) lr)) r
-onlyL' bl@B9{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (pushWith (S1 ll) (plugR d cap1) $ \e -> LG bl e lr)) r
-onlyL' bl@B9{} (D2 (Cap (LY ll d lr) cap1) r)          f = f $ D2 (Triple (pushWith (S1 ll) (plugL cap1 d) (\e -> LG bl e lr))) r
-onlyL' bl@B9{} (D2 (Triple (LG ll d lr)) r)            f = f $ D2 (Triple (pushWith (S1 ll) d (\e -> LG bl e lr))) r
-onlyL' bl@B9{} (D2 (Triple (LR ll d lr)) r)            f = f $ D2 (Triple (pushWith (S1 ll) d (\e -> LG bl e lr))) r
-onlyL' bl      (D2 (Triple (L0 ll lr)) r)              f = f $ D2 (Triple (L0 (catenateB bl ll) lr)) r
-onlyL' bl@B1{} (D2 (Triple (LR ll D0 lr)) r)           f = f $ D2 (Triple (L0 (catenateB bl ll) lr)) r
-onlyL' bl@B1{} (D2 (Triple (LR ll (D2 lt rt) lr)) r) f = f $ case uncap rt of ViewCap rt2 cap2 -> D2 (Cap (LO (catenateB bl ll) (D2 lt rt2) lr) cap2) r
-onlyL' bl@B1{} (D2 (Triple (LR ll (DOL ot) lr)) r)   f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 (Cap (LO (catenateB bl ll) (DOR ot2) lr) cap2) r
-onlyL' bl@B1{} (D2 (Triple (LR ll (DOR ot) lr)) r)   f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 (Cap (LO (catenateB bl ll) (DOR ot2) lr) cap2) r
-onlyL' bl@B2{} (D2 (Triple (LR ll D0 lr)) r)         f = f $ D2 (Triple (L0 (catenateB bl ll) lr)) r
-onlyL' bl@B2{} (D2 (Triple (LR ll (D2 lt rt) lr)) r) f = f $ case uncap lt of ViewCap lt2 cap2 -> D2 (Cap (LY (catenateB bl ll) (D2 lt2 rt) lr) cap2) r
-onlyL' bl@B2{} (D2 (Triple (LR ll (DOL ot) lr)) r)   f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 (Cap (LY (catenateB bl ll) (DOL ot2) lr) cap2) r
-onlyL' bl@B2{} (D2 (Triple (LR ll (DOR ot) lr)) r)   f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 (Cap (LY (catenateB bl ll) (DOL ot2) lr) cap2) r
-onlyL' bl@B3{} (D2 (Triple (LR ll d lr)) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
-onlyL' bl@B4{} (D2 (Triple (LR ll d lr)) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
-onlyL' bl@B5{} (D2 (Triple (LR ll d lr)) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
-onlyL' bl@B6{} (D2 (Triple (LR ll d lr)) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
-onlyL' bl@B7{} (D2 (Triple (LR ll d lr)) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
-onlyL' bl@B1{} (D2 (Cap (LO ll (D2 lt rt) lr) cap1) r) f = f $ case uncap lt of ViewCap lt2 cap2 -> D2 (Cap (LY (catenateB bl ll) (D2 lt2 (cap rt cap1)) lr) cap2) r
-onlyL' bl@B1{} (D2 (Cap (LO ll (DOR ot) lr) cap1) r)   f = f $ D2 (Cap (LY (catenateB bl ll) (DOL ot) lr) cap1) r
-onlyL' bl@B2{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl@B3{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl@B4{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl@B5{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl@B6{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl@B7{} (D2 (Cap (LO ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugR d cap1
-onlyL' bl      (D2 (Cap (LY ll d lr) cap1) r)          f = f $ D2 (Triple (LG (catenateB bl ll) d2 lr)) r where d2 = plugL cap1 d
-onlyL' bl      (D2 (Triple (LG ll d lr)) r)            f = f $ D2 (Triple (LG (catenateB bl ll) d lr)) r
+onlyL' :: Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q j k -> Cap LeftTriple (Closed cl1) q i j -> (forall cl2. Cap LeftTriple (Closed cl2) q i k -> g) -> g
+onlyL' bl@B8{} (Triple (L0 ll lr))              f = f $ Triple (LG bl (push (S1 ll) D0) lr)
+onlyL' bl@B8{} (Cap (LO ll d lr) cap1)          f = f $ Triple (pushWith (S1 ll) (plugR d cap1) (\e -> LG bl e lr))
+onlyL' bl@B8{} (Cap (LY ll d lr) cap1)          f = f $ Triple (pushWith (S1 ll) (plugL cap1 d) (\e -> LG bl e lr))
+onlyL' bl@B8{} (Triple (LG ll d lr))            f = f $ Triple (pushWith (S1 ll) d (\e -> LG bl e lr))
+onlyL' bl@B8{} (Triple (LR ll d lr))            f = f $ Triple (pushWith (S1 ll) d (\e -> LG bl e lr))
+onlyL' bl@B9{} (Triple (L0 ll lr))              f = f $ Triple (LG bl (push (S1 ll) D0) lr)
+onlyL' bl@B9{} (Cap (LO ll d lr) cap1)          f = f $ Triple (pushWith (S1 ll) (plugR d cap1) $ \e -> LG bl e lr)
+onlyL' bl@B9{} (Cap (LY ll d lr) cap1)          f = f $ Triple (pushWith (S1 ll) (plugL cap1 d) (\e -> LG bl e lr))
+onlyL' bl@B9{} (Triple (LG ll d lr))            f = f $ Triple (pushWith (S1 ll) d (\e -> LG bl e lr))
+onlyL' bl@B9{} (Triple (LR ll d lr))            f = f $ Triple (pushWith (S1 ll) d (\e -> LG bl e lr))
+onlyL' bl      (Triple (L0 ll lr))              f = f $ Triple (L0 (catenateB bl ll) lr)
+onlyL' bl@B1{} (Triple (LR ll D0 lr))           f = f $ Triple (L0 (catenateB bl ll) lr)
+onlyL' bl@B1{} (Triple (LR ll (D2 lt rt) lr))   f = f $ case uncap rt of ViewCap rt2 cap2 -> Cap (LO (catenateB bl ll) (D2 lt rt2) lr) cap2
+onlyL' bl@B1{} (Triple (LR ll (DOL ot) lr))     f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (LO (catenateB bl ll) (DOR ot2) lr) cap2
+onlyL' bl@B1{} (Triple (LR ll (DOR ot) lr))     f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (LO (catenateB bl ll) (DOR ot2) lr) cap2
+onlyL' bl@B2{} (Triple (LR ll D0 lr))           f = f $ Triple (L0 (catenateB bl ll) lr)
+onlyL' bl@B2{} (Triple (LR ll (D2 lt rt) lr))   f = f $ case uncap lt of ViewCap lt2 cap2 -> Cap (LY (catenateB bl ll) (D2 lt2 rt) lr) cap2
+onlyL' bl@B2{} (Triple (LR ll (DOL ot) lr))     f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (LY (catenateB bl ll) (DOL ot2) lr) cap2
+onlyL' bl@B2{} (Triple (LR ll (DOR ot) lr))     f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (LY (catenateB bl ll) (DOL ot2) lr) cap2
+onlyL' bl@B3{} (Triple (LR ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
+onlyL' bl@B4{} (Triple (LR ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
+onlyL' bl@B5{} (Triple (LR ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
+onlyL' bl@B6{} (Triple (LR ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
+onlyL' bl@B7{} (Triple (LR ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
+onlyL' bl@B1{} (Cap (LO ll (D2 lt rt) lr) cap1) f = f $ case uncap lt of ViewCap lt2 cap2 -> Cap (LY (catenateB bl ll) (D2 lt2 (cap rt cap1)) lr) cap2
+onlyL' bl@B1{} (Cap (LO ll (DOR ot) lr) cap1)   f = f $ Cap (LY (catenateB bl ll) (DOL ot) lr) cap1
+onlyL' bl@B2{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl@B3{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl@B4{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl@B5{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl@B6{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl@B7{} (Cap (LO ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugR d cap1
+onlyL' bl      (Cap (LY ll d lr) cap1)          f = f $ Triple (LG (catenateB bl ll) d2 lr) where d2 = plugL cap1 d
+onlyL' bl      (Triple (LG ll d lr))            f = f $ Triple (LG (catenateB bl ll) d lr)
 
 onlyR :: Cap RightTriple (Closed Green) q j k -> Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q i j -> Cap RightTriple (Closed Green) q i k
 onlyR (Triple (R0 rl rr))              br@B8{} = Triple (RG rl (inject D0 (S1 rr)) br)
@@ -571,44 +568,41 @@ onlyR (Cap (RO rl d rr) cap1)          br@B7{} = Triple (RG rl d2 (catenateB rr 
 onlyR (Cap (RY rl d rr) cap1)          br      = Triple (RG rl d2 (catenateB rr br)) where d2 = plugL cap1 d
 onlyR (Triple (RG rl d rr))            br      = Triple (RG rl d (catenateB rr br))
 
-onlyR' :: Deque (Closed cl1) (Closed cr1) q j k -> Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q i j -> (forall cl2 cr2. Deque (Closed cl2) (Closed cr2) q i k -> g) -> g
-onlyR' D0 _ _ = error "Impossible"
-onlyR' DOL{} _ _ = error "Impossible"
-onlyR' DOR{} _ _ = error "Impossible"
-onlyR' (D2 l (Triple (R0 rl rr)))              br@B8{} f = f $ D2 l (Triple (RG rl (inject D0 (S1 rr)) br))
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B8{} f = f $ D2 l (Triple (injectWith (plugR d cap1) (S1 rr) $ \e -> RG rl e br))
-onlyR' (D2 l (Cap (RY rl d rr) cap1))          br@B8{} f = f $ D2 l (Triple (injectWith (plugL cap1 d) (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (RG rl d rr)))            br@B8{} f = f $ D2 l (Triple (injectWith d (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (RR rl d rr)))            br@B8{} f = f $ D2 l (Triple (injectWith d (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (R0 rl rr)))              br@B9{} f = f $ D2 l (Triple (RG rl (inject D0 (S1 rr)) br))
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B9{} f = f $ D2 l (Triple (injectWith (plugR d cap1) (S1 rr) $ \e -> RG rl e br))
-onlyR' (D2 l (Cap (RY rl d rr) cap1))          br@B9{} f = f $ D2 l (Triple (injectWith (plugL cap1 d) (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (RG rl d rr)))            br@B9{} f = f $ D2 l (Triple (injectWith d (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (RR rl d rr)))            br@B9{} f = f $ D2 l (Triple (injectWith d (S1 rr) (\e -> RG rl e br)))
-onlyR' (D2 l (Triple (R0 rl rr)))              br      f = f $ D2 l (Triple (R0 rl (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl D0 rr)))         br@B1{} f = f $ D2 l (Triple (R0 rl (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl (D2 lt rt) rr))) br@B1{} f = f $ case uncap rt of ViewCap rt2 cap2 -> D2 l (Cap (RO rl (D2 lt rt2) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl (DOR ot) rr)))   br@B1{} f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 l (Cap (RO rl (DOR ot2) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl (DOL ot) rr)))   br@B1{} f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 l (Cap (RO rl (DOR ot2) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl D0 rr)))         br@B2{} f = f $ D2 l (Triple (R0 rl (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl (D2 lt rt) rr))) br@B2{} f = f $ case uncap lt of ViewCap lt2 cap2 -> D2 l (Cap (RY rl (D2 lt2 rt) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl (DOR ot) rr)))   br@B2{} f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 l (Cap (RY rl (DOL ot2) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl (DOL ot) rr)))   br@B2{} f = f $ case uncap ot of ViewCap ot2 cap2 -> D2 l (Cap (RY rl (DOL ot2) (catenateB rr br)) cap2)
-onlyR' (D2 l (Triple (RR rl d rr)))          br@B3{} f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl d rr)))          br@B4{} f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl d rr)))          br@B5{} f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl d rr)))          br@B6{} f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
-onlyR' (D2 l (Triple (RR rl d rr)))          br@B7{} f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
-onlyR' (D2 l (Cap (RO rl (D2 lt rt) rr) cap1)) br@B1{} f = f $ case uncap lt of ViewCap lt2 cap2 -> D2 l (Cap (RY rl (D2 lt2 (cap rt cap1)) (catenateB rr br)) cap2)
-onlyR' (D2 l (Cap (RO rl (DOR ot) rr) cap1))   br@B1{} f = f $ D2 l (Cap (RY rl (DOL ot) (catenateB rr br)) cap1)
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B2{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B3{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B4{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B5{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B6{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RO rl d rr) cap1))          br@B7{} f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugR d cap1
-onlyR' (D2 l (Cap (RY rl d rr) cap1))          br      f = f $ D2 l (Triple (RG rl d2 (catenateB rr br))) where d2 = plugL cap1 d
-onlyR' (D2 l (Triple (RG rl d rr)))            br      f = f $ D2 l (Triple (RG rl d (catenateB rr br)))
+onlyR' :: Cap RightTriple (Closed cr1) q j k -> Buffer k1 k2 k3 k4 k5 k6 k7 k8 k9 q i j -> (forall cr2. Cap RightTriple (Closed cr2) q i k -> g) -> g
+onlyR' (Triple (R0 rl rr))              br@B8{} f = f $ Triple (RG rl (inject D0 (S1 rr)) br)
+onlyR' (Cap (RO rl d rr) cap1)          br@B8{} f = f $ Triple (injectWith (plugR d cap1) (S1 rr) $ \e -> RG rl e br)
+onlyR' (Cap (RY rl d rr) cap1)          br@B8{} f = f $ Triple (injectWith (plugL cap1 d) (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (RG rl d rr))            br@B8{} f = f $ Triple (injectWith d (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (RR rl d rr))            br@B8{} f = f $ Triple (injectWith d (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (R0 rl rr))              br@B9{} f = f $ Triple (RG rl (inject D0 (S1 rr)) br)
+onlyR' (Cap (RO rl d rr) cap1)          br@B9{} f = f $ Triple (injectWith (plugR d cap1) (S1 rr) $ \e -> RG rl e br)
+onlyR' (Cap (RY rl d rr) cap1)          br@B9{} f = f $ Triple (injectWith (plugL cap1 d) (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (RG rl d rr))            br@B9{} f = f $ Triple (injectWith d (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (RR rl d rr))            br@B9{} f = f $ Triple (injectWith d (S1 rr) (\e -> RG rl e br))
+onlyR' (Triple (R0 rl rr))              br      f = f $ Triple (R0 rl (catenateB rr br))
+onlyR' (Triple (RR rl D0 rr))           br@B1{} f = f $ Triple (R0 rl (catenateB rr br))
+onlyR' (Triple (RR rl (D2 lt rt) rr))   br@B1{} f = f $ case uncap rt of ViewCap rt2 cap2 -> Cap (RO rl (D2 lt rt2) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl (DOR ot) rr))     br@B1{} f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (RO rl (DOR ot2) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl (DOL ot) rr))     br@B1{} f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (RO rl (DOR ot2) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl D0 rr))           br@B2{} f = f $ Triple (R0 rl (catenateB rr br))
+onlyR' (Triple (RR rl (D2 lt rt) rr))   br@B2{} f = f $ case uncap lt of ViewCap lt2 cap2 -> Cap (RY rl (D2 lt2 rt) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl (DOR ot) rr))     br@B2{} f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (RY rl (DOL ot2) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl (DOL ot) rr))     br@B2{} f = f $ case uncap ot of ViewCap ot2 cap2 -> Cap (RY rl (DOL ot2) (catenateB rr br)) cap2
+onlyR' (Triple (RR rl d rr))            br@B3{} f = f $ Triple (RG rl d (catenateB rr br))
+onlyR' (Triple (RR rl d rr))            br@B4{} f = f $ Triple (RG rl d (catenateB rr br))
+onlyR' (Triple (RR rl d rr))            br@B5{} f = f $ Triple (RG rl d (catenateB rr br))
+onlyR' (Triple (RR rl d rr))            br@B6{} f = f $ Triple (RG rl d (catenateB rr br))
+onlyR' (Triple (RR rl d rr))            br@B7{} f = f $ Triple (RG rl d (catenateB rr br))
+onlyR' (Cap (RO rl (D2 lt rt) rr) cap1) br@B1{} f = f $ case uncap lt of ViewCap lt2 cap2 -> Cap (RY rl (D2 lt2 (cap rt cap1)) (catenateB rr br)) cap2
+onlyR' (Cap (RO rl (DOR ot) rr) cap1)   br@B1{} f = f $ Cap (RY rl (DOL ot) (catenateB rr br)) cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B2{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B3{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B4{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B5{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B6{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RO rl d rr) cap1)          br@B7{} f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugR d cap1
+onlyR' (Cap (RY rl d rr) cap1)          br      f = f $ Triple (RG rl d2 (catenateB rr br)) where d2 = plugL cap1 d
+onlyR' (Triple (RG rl d rr))            br      f = f $ Triple (RG rl d (catenateB rr br))
 
 fixLeft :: Deque (Closed Green) (Closed Green) q i j -> Cap LeftTriple (Closed Green) q i j
 fixLeft d = case d of
